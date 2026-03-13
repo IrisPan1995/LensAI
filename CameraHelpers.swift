02006e85d3475e9cb28dc24fa4df8d8e3bb556ee
IrisPan1995 <hankingetsu@163.com>
2026-03-13 17:16:26 +0800

    123

--- CameraHelpers.swift
+++ CameraHelpers.swift
@@ -17,7 +17,7 @@ struct CameraPreview: UIViewRepresentable {
             view.layer.addSublayer(layer)
             DispatchQueue.global(qos: .userInitiated).async { session.startRunning() }
         }
-        return view
+        return view//1111111
     }
 
     func updateUIView(_ view: UIView, context: Context) {}
